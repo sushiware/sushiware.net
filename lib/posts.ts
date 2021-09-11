e9e774fb8d7e6cfcf302b51e8e10d433e136e929
nas <y.softvalley@hey.com>
2021-09-12 08:50:02 +0900

    remove

--- lib/posts.ts
+++ lib/posts.ts
@@ -2,7 +2,7 @@ import fs from 'fs';
 import path from 'path';
 
 import matter from 'gray-matter';
-import remark from 'remark';
+import { remark } from 'remark';
 import highlight from 'remark-highlight.js';
 import html from 'remark-html';
 
