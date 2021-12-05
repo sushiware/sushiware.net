3a801f58f8474452cab5014577516a46dfa368d3
nas <y.softvalley@hey.com>
2021-12-06 00:59:29 +0900

    rewrite repl code by ts

---
+++ deno/runMotokoRepl.ts
@@ -0,0 +1,189 @@
+export const changeCodeBlock = (
+  preTags: HTMLCollectionOf<HTMLPreElement>,
+  mo: Motoko
+) => {
+  Array.from(preTags)
+    .filter((tag) => {
+      const child = tag.firstChild as HTMLElement;
+
+      return (
+        child &&
+        child.tagName === "CODE" &&
+        child.classList.contains("language-motoko")
+      );
+    })
+    .forEach((tag) => {
+      tag.classList.add("motoko");
+
+      appendRun(tag, extractConfig(tag), mo);
+    });
+};
+
+type jsonRespose = {
+  files: [File];
+};
+
+export const addPackage = async (
+  name: string,
+  repo: string,
+  version: string,
+  dir: string,
+  preTags: HTMLCollectionOf<HTMLPreElement>,
+  mo: Motoko
+) => {
+  const metaUrl = `https://data.jsdelivr.com/v1/package/gh/${repo}@${version}/flat`;
+  const baseUrl = `https://cdn.jsdelivr.net/gh/${repo}@${version}`;
+  const response = await fetch(metaUrl);
+  const json = (await response.json()) as jsonRespose;
+  const promises = [];
+  const fetchedFiles = [];
+  json.files.filter(
+    (f) => f.name.startsWith(`/${dir}/`) && /\.mo$/.test(f.name)
+  );
+  for (const f of json.files) {
+    if (f.name.startsWith(`/${dir}/`) && /\.mo$/.test(f.name)) {
+      const promise = (async () => {
+        const content = await (await fetch(baseUrl + f.name)).text();
+        const stripped = name + f.name.slice(dir.length + 1);
+        fetchedFiles.push(stripped);
+        mo.saveFile(stripped, content);
+      })();
+      promises.push(promise);
+    }
+  }
+
+  Promise.all(promises).then(() => {
+    mo.addPackage(name, name + "/");
+    console.log(`Loaded motoko library "${name}"`);
+    changeCodeBlock(preTags, mo);
+  });
+};
+
+const extractConfig = (pre: HTMLPreElement): Config => {
+  const div = pre?.parentNode?.parentNode;
+
+  const name = (div as Element).getAttribute("id") + ".mo" || "";
+
+  const classList = Array.from((div as HTMLElement).classList);
+
+  const include = classList
+    .filter((c) => c.startsWith("include"))
+    .reduce((acc: string[], c: string) => acc.concat(c.split("_")), []);
+
+  const hook =
+    classList
+      .filter((c) => c.startsWith("hook"))
+      .find((c) => c.split("_")[1]) || "";
+
+  return {
+    name: name,
+    include: include,
+    hook: hook,
+    isRun: classList.some((c) => c === "run"),
+    noRepl: classList.some((c) => c === "no-repl"),
+  };
+};
+
+const saveIncluded = (mo: Motoko, include: string[]) => {
+  return include.reduce((codes: { [key: string]: string }, id: string) => {
+    const node = document.getElementById(id);
+
+    const code =
+      node
+        ?.querySelector("div.content")
+        ?.querySelector("pre")
+        ?.querySelector("code")?.innerText || "";
+
+    const name = id + ".mo";
+
+    mo.saveFile(name, id);
+
+    codes[name] = code;
+
+    return codes;
+  }, {});
+};
+
+const appendRun = (preTag: HTMLPreElement, config: Config, mo: Motoko) => {
+  if (config.name) {
+    mo.saveFile(config.name, (preTag.firstChild as HTMLElement).innerText);
+  }
+
+  if (config.noRepl) {
+    return;
+  }
+
+  const target = document.createElement("div");
+
+  (preTag.parentNode as HTMLElement).insertBefore(target, preTag.nextSibling);
+
+  (target as HTMLElement).classList.add("run-motoko");
+
+  const button = document.createElement("button");
+  const output = document.createElement("div");
+
+  (output as HTMLElement).classList.add("listingblock");
+
+  if (config.isRun) {
+    output.innerHTML = "<pre>Loading...</pre>";
+  }
+
+  button.innerHTML = "<span>Run</span>";
+  button.classList.add("run-button");
+
+  target.appendChild(button);
+  target.appendChild(output);
+
+  button.addEventListener("click", () => {
+    saveIncluded(mo, config.include);
+
+    const code = (preTag.firstChild as HTMLElement).innerText;
+
+    const reader = config.name || "stdin";
+
+    mo.saveFile(reader, code);
+
+    // TODO: config.hookを無視しているので調整する
+    const out = mo.run(
+      config.include.map((s) => s + ".mo"),
+      reader
+    );
+
+    output.innerHTML = "";
+
+    if (out.stderr) {
+      const pre = document.createElement("pre");
+
+      pre.innerText = out.stderr;
+      pre.classList.add("repl-error");
+
+      output.appendChild(pre);
+    }
+
+    if (out.stdout) {
+      const pre = document.createElement("pre");
+
+      pre.innerText = out.stdout;
+
+      output.appendChild(pre);
+    }
+  });
+
+  if (config.isRun) {
+    button.click();
+  }
+};
+
+type Config = {
+  name: string;
+  include: string[];
+  hook: string;
+  isRun: boolean;
+  noRepl: boolean;
+};
+
+type Motoko = {
+  saveFile: (name: string, code: string) => void;
+  run: (files: string[], reader: string) => { stdout: string; stderr: string };
+  addPackage: (name: string, path: string) => void;
+};
---
+++ static/js/runMotokoRepl.bundle.js
@@ -0,0 +1,112 @@
+const changeCodeBlock1 = (preTags, mo)=>{
+    Array.from(preTags).filter((tag)=>{
+        const child = tag.firstChild;
+        return child && child.tagName === "CODE" && child.classList.contains("language-motoko");
+    }).forEach((tag)=>{
+        tag.classList.add("motoko");
+        appendRun(tag, extractConfig(tag), mo);
+    });
+};
+const addPackage1 = async (name, repo, version, dir, preTags, mo)=>{
+    const metaUrl = `https://data.jsdelivr.com/v1/package/gh/${repo}@${version}/flat`;
+    const baseUrl = `https://cdn.jsdelivr.net/gh/${repo}@${version}`;
+    const response = await fetch(metaUrl);
+    const json = await response.json();
+    const promises = [];
+    const fetchedFiles = [];
+    json.files.filter((f)=>f.name.startsWith(`/${dir}/`) && /\.mo$/.test(f.name)
+    );
+    for (const f of json.files){
+        if (f.name.startsWith(`/${dir}/`) && /\.mo$/.test(f.name)) {
+            const promise = (async ()=>{
+                const content = await (await fetch(baseUrl + f.name)).text();
+                const stripped = name + f.name.slice(dir.length + 1);
+                fetchedFiles.push(stripped);
+                mo.saveFile(stripped, content);
+            })();
+            promises.push(promise);
+        }
+    }
+    Promise.all(promises).then(()=>{
+        mo.addPackage(name, name + "/");
+        console.log(`Loaded motoko library "${name}"`);
+        changeCodeBlock1(preTags, mo);
+    });
+};
+const extractConfig = (pre)=>{
+    const div = pre?.parentNode?.parentNode;
+    const name = div.getAttribute("id") + ".mo" || "";
+    const classList = Array.from(div.classList);
+    const include = classList.filter((c)=>c.startsWith("include")
+    ).reduce((acc, c)=>acc.concat(c.split("_"))
+    , []);
+    const hook = classList.filter((c)=>c.startsWith("hook")
+    ).find((c)=>c.split("_")[1]
+    ) || "";
+    return {
+        name: name,
+        include: include,
+        hook: hook,
+        isRun: classList.some((c)=>c === "run"
+        ),
+        noRepl: classList.some((c)=>c === "no-repl"
+        )
+    };
+};
+const saveIncluded = (mo, include)=>{
+    return include.reduce((codes, id)=>{
+        const node = document.getElementById(id);
+        const code = node?.querySelector("div.content")?.querySelector("pre")?.querySelector("code")?.innerText || "";
+        const name = id + ".mo";
+        mo.saveFile(name, id);
+        codes[name] = code;
+        return codes;
+    }, {
+    });
+};
+const appendRun = (preTag, config, mo)=>{
+    if (config.name) {
+        mo.saveFile(config.name, preTag.firstChild.innerText);
+    }
+    if (config.noRepl) {
+        return;
+    }
+    const target = document.createElement("div");
+    preTag.parentNode.insertBefore(target, preTag.nextSibling);
+    target.classList.add("run-motoko");
+    const button = document.createElement("button");
+    const output = document.createElement("div");
+    output.classList.add("listingblock");
+    if (config.isRun) {
+        output.innerHTML = "<pre>Loading...</pre>";
+    }
+    button.innerHTML = "<span>Run</span>";
+    button.classList.add("run-button");
+    target.appendChild(button);
+    target.appendChild(output);
+    button.addEventListener("click", ()=>{
+        saveIncluded(mo, config.include);
+        const code = preTag.firstChild.innerText;
+        const reader = config.name || "stdin";
+        mo.saveFile(reader, code);
+        const out = mo.run(config.include.map((s)=>s + ".mo"
+        ), reader);
+        output.innerHTML = "";
+        if (out.stderr) {
+            const pre = document.createElement("pre");
+            pre.innerText = out.stderr;
+            pre.classList.add("repl-error");
+            output.appendChild(pre);
+        }
+        if (out.stdout) {
+            const pre = document.createElement("pre");
+            pre.innerText = out.stdout;
+            output.appendChild(pre);
+        }
+    });
+    if (config.isRun) {
+        button.click();
+    }
+};
+export { changeCodeBlock1 as changeCodeBlock };
+export { addPackage1 as addPackage };
